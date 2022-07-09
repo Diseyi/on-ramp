2a7b4cce0379ed010dec8c46fb75ab1400f27599
diseyi <filomiinaa@gmail.com>
2022-07-09 09:23:50 +0100

    feat: create balance modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-import ExchangeCoin from "./component/exchange-coin";
+import Balance from "./component/balance";
+
 
 const App = () => {
   return (
     <div className="">
-     <ExchangeCoin />
+     <Balance />
     </div>
   );
 };
